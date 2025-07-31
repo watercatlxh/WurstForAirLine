746f37d5397167b8f53a8e8b2b7db8c0f5bffd19
watercatlxh <101051763+watercatlxh@users.noreply.github.com>
2025-07-31 18:29:21 +0800

    [/] Delete 1.10.2 Support

--- ForgeWurst MC 1.10.2/gradlew eclipse.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -jar LaunchFix.jar "-Dfml.coreMods.load=net.wurstclient.forge.asm.WCoreMod" true
--- build-all.bat
+++ build-all.bat
@@ -1,3 +1,2 @@
 @echo off
 start /D "ForgeWurst MC 1.12.2" gradlew build
-start /D "ForgeWurst MC 1.10.2" gradlew build
